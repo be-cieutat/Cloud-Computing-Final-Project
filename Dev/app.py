652c6d2c9da21ef2adada4416a9ae948cd6b650e
boyan <be.cieutat@protonmail.com>
2023-03-15 13:17:00 +0100

    Frontend reworked

--- Dev/app.py
+++ Dev/app.py
@@ -21,5 +21,13 @@ app = Flask (__name__)
 def index():
     return render_template("index.html", results1 = [], results2 = [])
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
